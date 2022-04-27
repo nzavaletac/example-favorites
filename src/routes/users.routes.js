cf7ff0383b407ed3b48dede190f34d515eb479df
Noe Zavaleta C <nzavaletac@gmail.com>
2022-04-27 17:16:39 -0500

    Login finished and functional

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,7 +1,8 @@
-const { signup } = require("../controllers/users.controllers");
+const { signup, login } = require("../controllers/users.controllers");
 
 const router = require("express").Router();
 
 router.route("/local/signup").post(signup);
+router.route("/local/login").post(login);
 
 module.exports = router;
